323397cfc64c6144f3cc6758a9abfdece5e6fcf3
YehiaSAhmed <yahyasaidahmed706@gmail.com>
2022-11-04 20:18:41 +0200

    Updating app.py

--- app.py
+++ app.py
@@ -5,7 +5,55 @@ import streamlit as st
 from matplotlib import pyplot as plt
 import librosa.display
 from scipy.io import wavfile
-import plotly.express as px
+import pydub
+
+@st.cache
+def handle_uploaded_audio_file(uploaded_file):
+    a = pydub.AudioSegment.from_file(file=uploaded_file, format=uploaded_file.name.split(".")[-1])
+    channel_sounds = a.split_to_mono()
+    samples = [s.get_array_of_samples() for s in channel_sounds]
+    fp_arr = np.array(samples).T.astype(np.float32)
+    fp_arr /= np.iinfo(samples[0].typecode).max
+    return fp_arr[:, 0], a.frame_rate
+
+def plot_wave(y, sr):
+    fig, ax = plt.subplots()
+    img = librosa.display.waveshow(y, sr=sr, x_axis="time", ax=ax)
+    return plt.gcf()
+
+# def plot_transformation(y, sr, transformation_name):
+#     D = librosa.stft(y)  # STFT of y
+#     S_db = librosa.amplitude_to_db(np.abs(D), ref=np.max)
+#     fig, ax = plt.subplots()
+#     img = librosa.display.specshow(S_db, x_axis='time', y_axis='linear', ax=ax)
+#     ax.set(title=transformation_name)
+#     fig.colorbar(img, ax=ax, format="%+2.f dB")
+#     return plt.gcf()
+
+def spacing():
+    st.markdown("<br></br>", unsafe_allow_html=True)
+
+def plot_audio_transformations(y, sr):
+    # st.markdown(f"<h4 style='text-align: center;'>Original</h5>",unsafe_allow_html=True)
+    # st.pyplot(plot_transformation(y, sr, "Original"))
+    st.markdown(f"<h4 style='text-align: center;'>Wave Plot </h5>",unsafe_allow_html=True)
+    st.pyplot(plot_wave(y, sr))
+    y = y
+    sr = sr
+
+def action(file):
+    if file is not None:
+        y, sr = handle_uploaded_audio_file(file)
+    plot_audio_transformations(y, sr)
+
+def main():
+    music = st.sidebar.file_uploader(label="Select a file", type=[".wav"])
+    if music is not None:
+        action(music)
+        st.sidebar.audio(music)
+        song, sr = librosa.load(music)
+        fequency_domain(song,sr,'freq',0.1)
+
 def fequency_domain(signal,sr,title,f_ratio=1):
         freq=np.fft.fft(signal)
         freq_mag = np.absolute(freq)
@@ -14,27 +62,13 @@ def fequency_domain(signal,sr,title,f_ratio=1):
         fig.set_figheight(4)
         fig.set_figwidth(16)
         ax.set(title='Input audio')
-        line, = ax.plot(f, freq_mag)
-        #the_plot = st.pyplot(plt)
+        line = ax.plot(f, freq_mag)
+        st.markdown(f"<h4 style='text-align: center;'>Frequency Plot </h5>",unsafe_allow_html=True)
         st.pyplot(fig)
-        
-file_uploader = st.sidebar.file_uploader(label="",  type=[ 'wav'])
-if file_uploader is not None:  # File > 0 bytes
-        file_details = {"FileName": file_uploader.name,
-                        "FileType": file_uploader.type,
-                        "FileSize": file_uploader.size}
-        st.write(file_details)
-        #######################
-        # WAV UPLOADED FILE
-        #######################
-        if file_details['FileType'] == 'audio/wav':         
-                with open('Input/FileName', 'wb') as f:
-                                f.write(file_uploader.getbuffer())
-                                st.subheader("Input audio:")
-                                audio_file = open('Input/FileName', 'rb')
-                                audio_bytes = audio_file.read()
-                                st.audio(audio_bytes, format='audio/wav')
-                                song, sr = librosa.load('Input/FileName')
-                                fequency_domain(song,sr,'freq',0.1)
 
-        
+if __name__ == "__main__":
+    main()
+
+hide_st_style =""" <style> #MainMenu {visibility: hidden;} footer {visibility: hidden;} header {visibility: hidden;}
+                    </style>"""
+st.markdown(hide_st_style, unsafe_allow_html=True)
